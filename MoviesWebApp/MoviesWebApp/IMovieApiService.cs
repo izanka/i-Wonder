18e5a8a91f6d8f4d49587774fa26a429c0248525
izanka <42039017+izanka@users.noreply.github.com>
2021-04-22 17:11:58 +0530

    Creation of API Service and Movie model class

---
+++ MoviesWebApp/MoviesWebApp/IMovieApiService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoviesWebApp
+{
+    interface IMovieApiService
+    {
+        Task<List<MovieModel>> GetMovies(string title, string type);
+    }
+}
---
+++ MoviesWebApp/MoviesWebApp/MovieApiService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace MoviesWebApp
+{
+    public class MovieApiService : IMovieApiService
+    {
+        private static readonly HttpClient client;
+
+        static MovieApiService()
+        {
+            client = new HttpClient()
+            {
+                BaseAddress = new Uri("https://www.omdbapi.com")
+            };
+        }
+
+        
+        async public Task<List<MovieModel>> GetMovies(string title, string type)
+        {
+            var url = string.Format("/?t={0}&type={1}&apikey=4a249f8d", title, type);
+            var result = new List<MovieModel>();
+            var response = await client.GetAsync(url);
+            if (response.IsSuccessStatusCode)
+            {
+                var stringResponse = await response.Content.ReadAsStringAsync();
+
+                result = JsonSerializer.Deserialize<List<MovieModel>>(stringResponse,
+                    new JsonSerializerOptions() { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
+            }
+            else
+            {
+                throw new HttpRequestException(response.ReasonPhrase);
+            }
+
+            return result;
+        }
+    }
+}
